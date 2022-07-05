987b78d4751a3e61bb02860f1ba059f951846190
sasaafanasienka <you@example.comafanasienka@yandex.by>
2022-07-05 21:48:01 +0300

    feature_ thunk insaleed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main/Main'
 
 function App() {
-
-  if (!localStorage.getItem('record')) {
-    localStorage.setItem('record', 0)
-  }
 
   return (
     <div className="App">
