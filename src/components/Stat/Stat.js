950cef5c9ca3ab595ce4542fd61970300216ce60
sasaafanasienka <you@example.comafanasienka@yandex.by>
2022-07-18 22:27:04 +0300

    feature: add next level progressbar

--- src/components/Stat/Stat.js
+++ src/components/Stat/Stat.js
@@ -8,19 +8,34 @@ function Stat() {
     const score = useSelector(state => {return state.score})
     const currentRecord = useSelector(state => {return state.score.record})
     const currentLevel = useSelector(state => { return state.level.level })
+    const rows = useSelector(state => { return state.level.rows })
 
-    console.log(score)
+    console.log(rows)
+
+    const getProgress = () => {
+        return `${(rows % 15) / 15 * 100}%`
+        // return `linear-gradient(90deg, ` +
+        //     `rgba(255,238,0,1) 0%, ` +
+        //     `rgba(255,238,0,1) ${percent}%, ` +
+        //     `rgba(255,238,0,0) ${percent}%, ` +
+        //     `rgba(255,238,0,0) 100%)`
+    }
     
     return(
         <div className='Stat'>
             <p className='Stat__title'>SCORE:</p>
-            <p className='Stat__score'>{currentScore}</p>
+            <p className='Stat__score'>
+                <span className={`Stat__score-value ${score.animation}`}>{currentScore}</span>
+                <span className={`Stat__animate-plus ${score.animation}`}> + </span>
+                <span className={`Stat__animate ${score.animation}`}>{score.animationValue}</span>
+            </p>
             <p className='Stat__title'>BEST:</p>
             <p className='Stat__score'>{currentRecord}</p>
             <p className='Stat__title'>SPEED:</p>
             <p className='Stat__score'>{currentLevel}</p>
-            <div className={`Stat__animate ${score.animation}`}>
-                {score.animationValue}
+            <p className='Stat__title'>NEXT LEVEL:</p>
+            <div className='Stat__next-level-container'>
+                <div className='Stat__next-level-progress' style={{width: getProgress()}}></div>
             </div>
         </div>
         )
